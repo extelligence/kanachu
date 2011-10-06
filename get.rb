ebb73c10a91d45c4d8b855c2a056dbfc60b5c044
Takeshi Sato <master.extelligence@gmail.com>
2011-10-07 02:54:52 +0900

    時刻表データを正規化。

--- get.rb
+++ get.rb
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
-require 'rubygems'
 require 'pp'
 require 'kconv'
 require 'net/http'
@@ -15,12 +14,12 @@ class Crawler
     @user_agent = user_agent
   end
 
-  def raw_html
-    open(@url, 'User-Agent' => @user_agent).read.toutf8.gsub(/\r/, "\n")
-  end
-
   def doc
     Nokogiri::HTML(raw_html)
+  end
+
+  def raw_html
+    open(@url, 'User-Agent' => @user_agent).read.toutf8.gsub(/\r/, "\n")
   end
 end
 
@@ -29,27 +28,39 @@ class Parser
     start_hour = 5
     end_hour   = 26
     time_table_array = []
-    result_table = Hash.new
+    time_table_hours = Hash.new
 
     time_table_xpath = "/html/body/table[2]"
     html_time_table = doc.xpath(time_table_xpath)
     start_hour..end_hour.times do |i|
-      hoge = html_time_table.xpath("tr[#{i - 1}]/td").text
-      hoge.delete!("\n")
-      hoge = hoge.split
-      unless hoge[0].nil?
-        time_table_array << hoge
+      content = html_time_table.xpath("tr[#{i - 1}]/td").text
+      content.delete!("\n")
+      content = content.split
+      unless content[0].nil?
+        time_table_array << content
       end
     end
+
     time_table_array.each do |h|
-      result_table["#{h[0]}"] = h[2]
+      time_table_hours["#{h[0]}"] = h[2] unless h[2].nil?
     end
-    result_table
+    time_table_hours.delete('系統')
+    time_table_hours
+  end
+
+  def self.get_timetable(doc)
+    time_table_minutes = Hash.new
+    exec(doc).each do |k, v|
+      v = v.unpack("a2" * (v.size / 2))
+      time_table_minutes["#{k}"] = v
+    end
+    time_table_minutes
   end
 end
 
 if $0 == __FILE__
   url = "http://dia.kanachu.jp/bus/timetable?busstop=16064&pole=7&pole_seq=2&apply=2011/10/03&day=1"
   isehara = Crawler.new(url, USER_AGENT)
-  pp Parser.exec(isehara.doc)
+  time_table = Parser.get_timetable(isehara.doc)
+  pp time_table
 end
