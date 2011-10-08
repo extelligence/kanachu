9be30d041262a2ab4751c62d36f09afd0a083f2d
Takeshi Sato <master.extelligence@gmail.com>
2011-10-08 23:00:02 +0900

    最寄りのバス停に到着する複数路線の時刻を表示するようにした。

--- get.rb
+++ get.rb
@@ -46,6 +46,7 @@ class Parser
       time_table_hours["#{h[0]}"] = h[2] unless h[2].nil?
     end
     time_table_hours.delete('系統')
+    time_table_hours.delete('経由')
     time_table_hours
   end
 
@@ -65,25 +66,52 @@ end
 
 def get_bus_now?(time_table)
   if time_table.key?("#{now_hour}")
-    puts time_table["#{now_hour}"]
+    print time_table["#{now_hour}"]
   else
-    puts "この時間のバスはないよ！"
+    print "この時間のバスはないよ！"
   end
 end
 
 if $0 == __FILE__
-  url = "http://dia.kanachu.jp/bus/timetable?busstop=16064&pole=7&pole_seq=2&apply=2011/10/03&day=1"
+  #
+  # ■ユーザーの利用路線について
+  # ▼22:05以前の場合、どちらかに乗車する。
+  #   平86             [枝大島入口下車] 伊勢原駅南口行(大田経由)
+  #   平68             [枝大島入口下車] 愛甲石田駅行
+  # ▼22:05以降の場合、どれかに乗車する。
+  #   平63             [田村車庫下車]   田村車庫行(横内経由)
+  #   平65             [田村車庫下車]   田村車庫行(大島経由)
+  #   平67             [田村車庫下車]   田村車庫行(神明経由)
+  #   平51 平52 平60   [田村車庫下車]   田村車庫行(旧道経由)
+  #   平97             [大島住宅下車]   伊勢原駅南口行(平間・大島経由) ※19:45以降ないみたい。
+  #
   # URLパラメータ仕様
   # [pole]     行き先
   # [pole_seq] 経由
   # [day]      平日:1 土曜:2 休日:3
+  KANACHU_TIMETABLE_URL = [
+    {"url" => "http://dia.kanachu.jp/bus/timetable?busstop=16064&pole=4&pole_seq=2&apply=2011/10/03&day=1", "name" => "伊勢原駅南口行(大田経由)"},
+    {"url" => "http://dia.kanachu.jp/bus/timetable?busstop=16064&pole=4&pole_seq=7&apply=2011/10/03&day=1", "name" => "愛甲石田駅行"},
+    {"url" => "http://dia.kanachu.jp/bus/timetable?busstop=16064&pole=4&pole_seq=3&apply=2011/10/03&day=1", "name" => "田村車庫行(横内経由)"},
+    {"url" => "http://dia.kanachu.jp/bus/timetable?busstop=16064&pole=4&pole_seq=4&apply=2011/10/03&day=1", "name" => "田村車庫行(大島経由)"},
+    {"url" => "http://dia.kanachu.jp/bus/timetable?busstop=16064&pole=4&pole_seq=5&apply=2011/10/03&day=1", "name" => "田村車庫行(神明経由)"},
+    {"url" => "http://dia.kanachu.jp/bus/timetable?busstop=16064&pole=5&pole_seq=2&apply=2011/10/03&day=1", "name" => "田村車庫行(旧道経由)"},
+    {"url" => "http://dia.kanachu.jp/bus/timetable?busstop=16064&pole=4&pole_seq=6&apply=2011/10/03&day=1", "name" => "伊勢原駅南口行(平間・大島経由)"}
+  ]
+  timetables = []
 
-  isehara = Crawler.new(url, USER_AGENT)
-  isehara_timetable = Parser.get_timetable(isehara.doc)
-  #pp isehara_timetable
+  KANACHU_TIMETABLE_URL.each do |t|
+    html = Crawler.new(t['url'], USER_AGENT)
+    timetables << {"name" => t['name'], "time" => Parser.get_timetable(html.doc)}
+  end
+  #pp timetables
 
   puts '=' * 80
   puts "現在の時間: #{Time.now.strftime("%H:%M")}"
-  get_bus_now?(isehara_timetable)
+  print "\n"
+  timetables.each do |t|
+    print "#{t['name']}：  "
+    puts get_bus_now?(t['time'])
+  end
   puts '=' * 80
 end
