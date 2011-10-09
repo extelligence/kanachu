90f368d8e7d810d1da7bffe01205ebe69fca45db
Takeshi Sato <master.extelligence@gmail.com>
2011-10-09 16:23:01 +0900

    Railsをインストール。

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kanachu::Application.config.secret_token = '745330b260c1df23a35a385efd0961cf7131308a01422c693db8c004073af5c82544c91856e380d08f4efaaba2ad19098e58375ec619729e756c8745475ef940'
