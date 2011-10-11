5187d9dd665c334b8f2041fef69d4e1ba0f56b5f
Takeshi Sato <master.extelligence@gmail.com>
2011-10-12 07:28:00 +0900

    ビューの時刻表表示不具合を修正。

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+
+  def format_str_time(string_time)
+      hour_minutes = string_time.unpack("a2" * (string_time.size / 2))
+      "#{hour_minutes[0]}:#{hour_minutes[1]}"
+  end
 end
