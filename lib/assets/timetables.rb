d4e14ea771055a1d8f2c6024292eb0870cc1e228
Takeshi Sato <master.extelligence@gmail.com>
2011-12-02 22:49:12 +0900

    時刻表改正のため、修正。

--- lib/assets/timetables.rb
+++ lib/assets/timetables.rb
@@ -5,8 +5,8 @@ require 'kconv'
 require 'net/http'
 require 'nokogiri'
 require 'open-uri'
-require 'crawler'
-require 'parser'
+require './crawler'
+require './parser'
 
 USER_AGENT = 'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0)'
 RESULT_TIMETABLE_NUM = 10
@@ -29,13 +29,13 @@ class Timetables
   # [pole_seq] 経由
   # [day]      平日:1 土曜:2 休日:3
   KANACHU_TIMETABLE_URL = [
-    {"url" => "http://dia.kanachu.jp/bus/timetable?busstop=16064&pole=4&pole_seq=2&apply=2011/10/16&day=1", "name" => "伊勢原駅南口行", "via" => "大田経由", "boarding" => "4"},
-    {"url" => "http://dia.kanachu.jp/bus/timetable?busstop=16064&pole=4&pole_seq=7&apply=2011/10/16&day=1", "name" => "愛甲石田駅行", "via" => "", "boarding" => "4"},
-    {"url" => "http://dia.kanachu.jp/bus/timetable?busstop=16064&pole=4&pole_seq=3&apply=2011/10/16&day=1", "name" => "田村車庫行", "via" => "横内経由", "boarding" => "4"},
-    {"url" => "http://dia.kanachu.jp/bus/timetable?busstop=16064&pole=4&pole_seq=4&apply=2011/10/16&day=1", "name" => "田村車庫行", "via" => "大島経由", "boarding" => "4"},
-    {"url" => "http://dia.kanachu.jp/bus/timetable?busstop=16064&pole=4&pole_seq=5&apply=2011/10/16&day=1", "name" => "田村車庫行", "via" => "神明経由", "boarding" => "4"},
-    {"url" => "http://dia.kanachu.jp/bus/timetable?busstop=16064&pole=5&pole_seq=2&apply=2011/10/16&day=1", "name" => "田村車庫行", "via" => "旧道経由", "boarding" => "5"},
-    {"url" => "http://dia.kanachu.jp/bus/timetable?busstop=16064&pole=4&pole_seq=6&apply=2011/10/16&day=1", "name" => "伊勢原駅南口行", "via" => "平間・大島経由", "boarding" => "4"}
+    {"url" => "http://dia.kanachu.jp/bus/timetable?busstop=16064&pole=4&pole_seq=2&apply=2011/12/01&day=1", "name" => "伊勢原駅南口行", "via" => "大田経由", "boarding" => "4"},
+    {"url" => "http://dia.kanachu.jp/bus/timetable?busstop=16064&pole=4&pole_seq=7&apply=2011/12/01&day=1", "name" => "愛甲石田駅行", "via" => "", "boarding" => "4"},
+    {"url" => "http://dia.kanachu.jp/bus/timetable?busstop=16064&pole=4&pole_seq=3&apply=2011/12/01&day=1", "name" => "田村車庫行", "via" => "横内経由", "boarding" => "4"},
+    {"url" => "http://dia.kanachu.jp/bus/timetable?busstop=16064&pole=4&pole_seq=4&apply=2011/12/01&day=1", "name" => "田村車庫行", "via" => "大島経由", "boarding" => "4"},
+    {"url" => "http://dia.kanachu.jp/bus/timetable?busstop=16064&pole=4&pole_seq=5&apply=2011/12/01&day=1", "name" => "田村車庫行", "via" => "神明経由", "boarding" => "4"},
+    {"url" => "http://dia.kanachu.jp/bus/timetable?busstop=16064&pole=5&pole_seq=2&apply=2011/12/01&day=1", "name" => "田村車庫行", "via" => "旧道経由", "boarding" => "5"},
+    {"url" => "http://dia.kanachu.jp/bus/timetable?busstop=16064&pole=4&pole_seq=6&apply=2011/12/01&day=1", "name" => "伊勢原駅南口行", "via" => "平間・大島経由", "boarding" => "4"}
   ]
 
 
